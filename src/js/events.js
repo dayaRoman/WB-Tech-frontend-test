113a251d1031863a5200fd0cb7b8597feb1678d7
dayaRoman <semfaro957@gmail.com>
2022-03-29 12:39:54 +0300

    событие переключения слайдера

--- src/js/events.js
+++ src/js/events.js
@@ -3,14 +3,31 @@ import serverData from "./serverResponse.js";
 document.onload = serverData();
 
 const slider = document.querySelector(".slider");
+let sliderItems = slider.getElementsByClassName("slider__item");
 
-// slider.addEventListener("click", (event) => {
-//     let target = event.target;
-
-//     if (target.matches(".slider__item") || target.closest(".slider__item")) {
-//         if (target.matches(".slider__item")) {
-
-//         }
-//     }
-
-// });
+slider.addEventListener("click", function (event) {
+    let target = event.target;
+    //let sliderItems = this.querySelectorAll(".slider__item");
+    if (
+        target.classList.contains("slider__item") ||
+        target.closest(".slider__item")
+    ) {
+        if (target.classList.contains("slider__item")) {
+            Array.prototype.forEach.call(sliderItems, function (item) {
+                if (item.classList.contains("slider__item-active")) {
+                    item.classList.remove("slider__item-active");
+                }
+            });
+            target.classList.add("slider__item-active");
+        } else {
+            let parent = target.closest(".slider__item");
+            Array.prototype.forEach.call(sliderItems, function (item) {
+                if (item.classList.contains("slider__item-active")) {
+                    item.classList.remove("slider__item-active");
+                }
+            });
+            parent.classList.add("slider__item-active");
+        }
+    }
+    //console.log(sliderItems);
+});
